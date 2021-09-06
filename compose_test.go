a1870753b5e34975c530e1d6c11cab559431d3fb
Abhay Kumar <abhaya.kumar@razorpay.com>
2021-09-06 14:58:18 +0530

    separation of test package

--- compose_test.go
+++ compose_test.go
@@ -1,21 +1,23 @@
-package exprxfunc
+package exprxfunc_test
 
 import (
+
 	"testing"
 
+	"github.com/abhay-kumar/expr-x-func"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCompose_Sample1(t *testing.T) {
 	expression := "amount + fee - tax"
-	function := Compose(expression)
+	function := exprxfunc.Compose(expression)
 	actual := "sub(add(amount,fee),tax)"
 	assert.Equal(t, actual, function)
 }
 
 func TestCompose_Sample2(t *testing.T) {
 	expression := "amount * fee - tax + bonus / dividend"
-	function := Compose(expression)
+	function := exprxfunc.Compose(expression)
 	actual := "add(sub(mul(amount,fee),tax),div(bonus,dividend))"
 	assert.Equal(t, actual, function)
 }
--- decompose_test.go
+++ decompose_test.go
@@ -1,21 +1,22 @@
-package exprxfunc
+package exprxfunc_test
 
 import (
 	"testing"
 
+	"github.com/abhay-kumar/expr-x-func"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestDecompose_Sample1(t *testing.T) {
 	function := "sub(add(amount,fee),tax)"
-	expression := Decompose(function)
+	expression := exprxfunc.Decompose(function)
 	actual := "amount + fee - tax"
 	assert.Equal(t, actual, expression)
 }
 
 func TestDecompose_Sample2(t *testing.T) {
 	function := "add(sub(mul(amount,fee),tax),div(bonus,dividend))"
-	expression := Decompose(function)
+	expression := exprxfunc.Decompose(function)
 	actual := "amount * fee - tax + bonus / dividend"
 	assert.Equal(t, actual, expression)
 }
